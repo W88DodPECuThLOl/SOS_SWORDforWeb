595ee9b04e3fec1a0ab9872a716424477f56fb91
猫大名ねこ猫 <48146054+W88DodPECuThLOl@users.noreply.github.com>
2022-12-29 09:10:24 +0900

    WOPENのテスト追加
    ・WOPENのテストを追加
    ・テスト用のディスクイメージを追加
    などなど

--- test/mk.bat
+++ test/mk.bat
@@ -1,5 +1,12 @@
+REM AILZ80ASM https://github.com/AILight/AILZ80ASM
 AILZ80ASM.exe --input testPause.z80 --output testPause.bin -f
 AILZ80ASM.exe --input testPause.z80 --output testPause.lst -lst -f
-REM S-OS obj header
-COPY /B OBJ_HEADER.BIN + testPause.bin testPause.obj
+
+AILZ80ASM.exe --input testWopen.z80 --output testWopen.bin -f
+AILZ80ASM.exe --input testWopen.z80 --output testWopen.lst -lst -f
+
+REM hudisk https://github.com/BouKiCHi/HuDisk
+hudisk.exe -a SOS_TEST.d88 testPause.bin --read 3000 --go 3000
+hudisk.exe -a SOS_TEST.d88 testWopen.bin --read 3000 --go 3000
+
 pause
