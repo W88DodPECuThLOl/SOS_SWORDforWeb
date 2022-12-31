20c70bb62a05241bc625b59c98ab8dc3d809bff4
猫大名ねこ猫 <48146054+W88DodPECuThLOl@users.noreply.github.com>
2023-01-01 02:48:54 +0900

    CTCテスト追加

--- test/mk.bat
+++ test/mk.bat
@@ -8,9 +8,13 @@ AILZ80ASM.exe --input testWopen.z80 --output testWopen.lst -lst -f
 AILZ80ASM.exe --input testRopen.z80 --output testRopen.bin -f
 AILZ80ASM.exe --input testRopen.z80 --output testRopen.lst -lst -f
 
+AILZ80ASM.exe --input testCTC.z80 --output testCTC.bin -f
+AILZ80ASM.exe --input testCTC.z80 --output testCTC.lst -lst -f
+
 REM hudisk https://github.com/BouKiCHi/HuDisk
 hudisk.exe -a SOS_TEST.d88 testPause.bin --read 3000 --go 3000
 hudisk.exe -a SOS_TEST.d88 testWopen.bin --read 3000 --go 3000
 hudisk.exe -a SOS_TEST.d88 testRopen.bin --read 3000 --go 3000
+hudisk.exe -a SOS_TEST.d88 testCTC.bin   --read 8000 --go 8000
 
 pause
